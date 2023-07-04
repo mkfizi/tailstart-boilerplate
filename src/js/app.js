54f017b1e2cc5711d8336a51af609c3d00c85ea0
Khafizi Noh <khafizi.noh@gmail.com>
2023-07-05 01:16:49 +0800

    Restructure `app.js` functions for more conventional standards

--- src/js/app.js
+++ src/js/app.js
@@ -19,6 +19,10 @@ app.elements = {
     footerAppVersion: document.getElementById('footer-app-version'),
 };
 
+app.config = {
+    isMenuActive: false
+}
+
 app.init = () => {
     app.view.init();
     app.event.init();
@@ -34,62 +38,64 @@ app.event = {
         const target = event.target;
 
         if (target.closest('[id="dark-mode-toggle"]')) {
-            app.view.toggleDarkMode();
-        } else if (target.closest('[id="navbar-menu-toggle"]')) {
-            app.view.toggleNavbarMenu();
+            app.view.darkMode.toggle();
         }
     },
 
     handleWindowResize: () => {
-        app.view.toggleViewportHeight();
+        app.view.footer.toggle();
     }
 };
 
 app.view = {
     init: () => {
-        app.view.toggleViewportHeight();
-        app.view.toggleAppInfo();
+        app.view.viewportHeight.toggle();
+        app.view.footer.toggle();
     },
 
-    // Toggle the height of the viewport. This is a workaround fix for [viewport height issue on mobile browsers](https://stackoverflow.com/questions/37112218/css3-100vh-not-constant-in-mobile-browser) 
-    toggleViewportHeight: () => {
-        document.documentElement.style.setProperty('--vh', (window.innerHeight * 0.01) + 'px');
-    },
-
-    // Toggle dark mode based on value in 'localStorage.theme'
-    toggleDarkMode: () => {
-        app.util.toggleTransition();
-
-        const isLightMode = localStorage.theme === 'light' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: light)').matches);
-        localStorage.theme = isLightMode ? 'dark' : 'light';
-        document.documentElement.classList.toggle('dark', isLightMode);
-    },
-
-    // Toogle footer data for year and app name
-    toggleAppInfo: () => {
-        if (app.elements.footerYear) {
-            app.elements.footerYear.innerHTML = new Date().getFullYear();
+    viewportHeight: {
+        toggle: () => {
+            document.documentElement.style.setProperty('--vh', (window.innerHeight * 0.01) + 'px');
         }
+    },
 
-        if (app.elements.footerAppName) {
-            app.elements.footerAppName.innerHTML = app.name;
+    darkMode: {
+        toggle: () => {
+            app.util.transition.toggle();
+
+            const isLightMode = localStorage.theme === 'light' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: light)').matches);
+            localStorage.theme = isLightMode ? 'dark' : 'light';
+            document.documentElement.classList.toggle('dark', isLightMode);
         }
+    },
 
-        if (app.elements.footerAppVersion) {
-            app.elements.footerAppVersion.innerHTML = app.version;
+    footer: {
+        toggle: () => {
+            if (app.elements.footerYear) {
+                app.elements.footerYear.innerHTML = new Date().getFullYear();
+            }
+    
+            if (app.elements.footerAppName) {
+                app.elements.footerAppName.innerHTML = app.name;
+            }
+            
+            if (app.elements.footerAppVersion) {
+                app.elements.footerAppVersion.innerHTML = app.version;
+            }
         }
     }
 };
 
 app.util = {
-    // Toggle CSS transitions for smoother element transitions
-    toggleTransition: () => {
-        const transitions = document.querySelectorAll('.transition, .transition-all, .transition-colors, .transition-opacity, .transition-shadow, .transition-transform');
-        for (const transition of transitions) {
-            transition.classList.add('transition-none');
-            setTimeout(() => {
-                transition.classList.remove('transition-none');
-            }, 150);
+    transition: {
+        toggle: () => {
+            const transitions = document.querySelectorAll('.transition, .transition-all, .transition-colors, .transition-opacity, .transition-shadow, .transition-transform');
+            for (const transition of transitions) {
+                transition.classList.add('transition-none');
+                setTimeout(() => {
+                    transition.classList.remove('transition-none');
+                }, 100);
+            }
         }
     }
 };
