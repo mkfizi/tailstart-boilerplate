104f06516b0e8b15ae8fdb07111b45b141093552
Khafizi Noh <khafizi.noh@gmail.com>
2023-07-13 03:43:58 +0800

    Update `app.js` to use IIFE and rewrite `app` object

--- src/js/app.js
+++ src/js/app.js
@@ -8,102 +8,91 @@
 (function () {
     'use strict';
 
-    const app = {
-        name: 'Tailstart Kit - Boilerplate',
-        version:'0.2.0',
-        
-        elements: {
-            id: {
-                darkMode: {
-                    toggle: 'darkmode-toggle'
-                }
-            },
+    const app = {};
 
-            node: {
-                footer: {
-                    year: document.getElementById('footer-year'),
-                    appName: document.getElementById('footer-app-name'),
-                    appVersion: document.getElementById('footer-app-version')
-                }
-            }
-        },
+    app.name = 'Tailstart Kit - Boilerplate';
+    app.version = '0.2.0';
 
-        events: {
-            document: {
-                click: event => {
-                    const target = event.target;
-                    if (target.closest(`[id=${app.elements.id.darkMode.toggle}]`)) {
-                        app.views.darkMode.toggle();
-                    }
-                }
-            },
-
-            window: {
-                resize: () => {
-                    app.views.viewportHeight.toggle();
-                }
-            },
-
-            init: () => {
-                document.addEventListener('click', app.events.document.click);
-                window.addEventListener('resize', app.events.window.resize);
-            }
-        },
-
-        views: {
-            // Workaround fix to handle viewport height issue on mobile browsers
+    app.element = {
+        darkModeToggle: document.getElementById('dark-mode-toggle'),
+        footerCurrentYear: document.getElementById('footer-year'),
+        footerAppName: document.getElementById('footer-app-name'),
+        footerAppVersion: document.getElementById('footer-app-version'),
+    }
+    
+    app.view = {
+        // Workaround fix to handle viewport height issue on mobile browsers
+        viewportHeight: {
             // https://stackoverflow.com/questions/37112218/css3-100vh-not-constant-in-mobile-browser
-            viewportHeight: {
-                toggle: () => {
-                    document.documentElement.style.setProperty('--vh', (window.innerHeight * 0.01) + 'px');
+            update: () => {
+                document.documentElement.style.setProperty('--vh', (window.innerHeight * 0.01) + 'px');
+            }
+        },
+
+        footer: {
+            // Update the footer with current year, app name and version
+            update: () => {
+                if (app.element.footerCurrentYear) {
+                    app.element.footerCurrentYear.innerHTML = new Date().getFullYear();
                 }
-            },
 
-            darkMode: {
-                // Toggle dark mode
-                toggle: () => {
-                    // Toggle transition classes to prevent FOUC
-                    const transitions = document.querySelectorAll('.transition, .transition-all, .transition-colors, .transition-opacity, .transition-shadow, .transition-transform');
-                    for (const transition of transitions) {
-                        transition.classList.add('transition-none');
-                        setTimeout(() => {
-                            transition.classList.remove('transition-none');
-                        }, 100);
-                    }
-        
-                    const isDarkMode = localStorage.theme === 'light' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: light)').matches);
-                    localStorage.theme = isDarkMode ? 'dark' : 'light';
-                    document.documentElement.classList.toggle('dark', isDarkMode);
+                if (app.element.footerAppName) {
+                    app.element.footerAppName.innerHTML = app.name;
                 }
-            },
-
-            footer: {
-                // Update footer information
-                toggle: () => {
-                    if (app.elements.node.footer.year) {
-                        app.elements.node.footer.year.innerHTML = new Date().getFullYear();
-                    }
-
-                    if (app.elements.node.footer.appName) {
-                        app.elements.node.footer.appName.innerHTML = app.name;
-                    }
-
-                    if (app.elements.node.footer.appVersion) {
-                        app.elements.node.footer.appVersion.innerHTML = app.version;
-                    }
+                
+                if (app.element.footerAppName) {
+                    app.element.footerAppVersion.innerHTML = app.version;
                 }
-            },
+            }
+        },
 
-            init: () => {
-                app.views.viewportHeight.toggle();
-                app.views.footer.toggle();
+
+        darkMode: {
+            // Toggle dark mode
+            toggle: () => {
+                const isDarkMode = localStorage.theme === 'light' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: light)').matches);
+                localStorage.theme = isDarkMode ? 'dark' : 'light';
+                document.documentElement.classList.toggle('dark', isDarkMode);
+            }
+        },
+
+        // Initialize view
+        init: () => {
+            app.view.viewportHeight.update();
+            app.view.footer.update();
+        }
+    }
+
+    app.event = {
+        document: {
+            // Handle document 'click event
+            click: event => {
+                const targetElement = event.target.closest('[id]');
+                if (targetElement) {
+                    const targetId = targetElement.getAttribute('id');
+                    if(document.getElementById(targetId) == app.element.darkModeToggle) {
+                        app.view.darkMode.toggle();
+                    } 
+                }
+            }
+        },
+
+        window: {
+            // Handle window 'resize' event
+            resize: () => {
+                app.view.viewportHeight.update();
             }
         },
 
         init: () => {
-            app.events.init();
-            app.views.init();
+            document.addEventListener('click', app.event.document.click);
+            window.addEventListener('resize', app.event.window.resize);
         }
+    },
+
+    app.init = () => {
+        app.view.init();
+        app.event.init();
     }
 
     app.init();
