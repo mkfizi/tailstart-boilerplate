d1c7691687f0b5e556968fbb95b4830024daa017
Khafizi Noh <khafizi.noh@gmail.com>
2023-07-15 02:15:07 +0800

    Rename all `update()` to `toggle()`

--- src/js/app.js
+++ src/js/app.js
@@ -24,14 +24,14 @@
         // Workaround fix to handle viewport height issue on mobile browsers
         viewportHeight: {
             // https://stackoverflow.com/questions/37112218/css3-100vh-not-constant-in-mobile-browser
-            update: () => {
+            toggle: () => {
                 document.documentElement.style.setProperty('--vh', (window.innerHeight * 0.01) + 'px');
             }
         },
 
         footer: {
-            // Update the footer with current year, app name and version
-            update: () => {
+            // Toggle footer content with current year, app name and version
+            toggle: () => {
                 if (app.element.footerCurrentYear) {
                     app.element.footerCurrentYear.innerHTML = new Date().getFullYear();
                 }
@@ -58,8 +58,8 @@
 
         // Initialize view
         init: () => {
-            app.view.viewportHeight.update();
-            app.view.footer.update();
+            app.view.viewportHeight.toggle();
+            app.view.footer.toggle();
         }
     }
 
@@ -70,6 +70,8 @@
                 const targetElement = event.target.closest('[id]');
                 if (targetElement) {
                     const targetId = targetElement.getAttribute('id');
+
+                    // If target is dark mode toggle
                     if (document.getElementById(targetId) == app.element.darkModeToggle) {
                         app.view.darkMode.toggle();
                     } 
@@ -80,7 +82,7 @@
         window: {
             // Handle window 'resize' event
             resize: () => {
-                app.view.viewportHeight.update();
+                app.view.viewportHeight.toggle();
             }
         },
 
